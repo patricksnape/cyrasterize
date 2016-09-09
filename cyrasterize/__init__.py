3d9b5023d0deea6140b3a602dc1d898dcda74a30
Patrick Snape <patricksnape@gmail.com>
2016-09-09 09:53:23 +0100

    Cyrasterize conflicts with VTK - import vtk first
    I don't know why, but if VTK isn't imported first then
    trying to create a rasterizer afterwards causes a sefault - so
    we just lazily try and import it to make sure that we don't see
    that bug. If vtk isn't installed then the ImportError is just
    swallowed.

--- cyrasterize/__init__.py
+++ cyrasterize/__init__.py
@@ -1,3 +1,11 @@
+try:
+    # There seems to be some bug that if VTK isn't imported before the
+    # rasterizer then we are unable to create an opengl context. So we just
+    # import it here if we can, in order to mitigate this
+    import vtk
+except ImportError:
+    pass
+
 from cyrasterize.base import CyRasterizer
 from .shader import FragmentShader, VertexShader
 
